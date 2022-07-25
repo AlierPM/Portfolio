f315a1f6194f3d8f69a78cecd5cf1803d1477c20
vigehi <vigehi2017@gmail.com>
2022-07-25 23:32:05 +0300

    Added js linter

---
+++ index.js
@@ -0,0 +1,7 @@
+function openmenunavigation() {
+    document.getElementById("git ").style.width = "100%";
+  }
+  
+  function closemenunavigation() {
+    document.getElementById("mob-nav").style.width = "0%";
+  }
